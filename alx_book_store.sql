e8e34dfc927ee524cd6ea65205c1795d68f0ad9c
otakukim <kimberlymjungwa@gmail.com>
2025-10-19 02:49:34 +0200

    Refactor SQL to use IF NOT EXISTS for table creation

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,49 +1,39 @@
-.sql‎
-+5
--5
-Lines changed: 5 additions & 5 deletions
-Original file line number	Diff line number	Diff line change
-@@ -1,13 +1,13 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
 
--- Creating Authors table
-CREATE TABLE IF NOT EXISTS Authors (
-CREATE TABLE  Authors (
+CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- Creating Books table
-CREATE TABLE IF NOT EXISTS Books (
-CREATE TABLE  Books (
+CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
-@@ -17,23 +17,23 @@ CREATE TABLE IF NOT EXISTS Books (
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Creating Customers table
-CREATE TABLE IF NOT EXISTS Customers (
-CREATE TABLE  Customers (
+CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL UNIQUE,
     address TEXT
 );
 
--- Creating Orders table
-CREATE TABLE IF NOT EXISTS Orders (
-CREATE TABLE  Orders (
+CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Creating Order_Details table
-CREATE TABLE IF NOT EXISTS Order_Details (
-CREATE TABLE  Order_Details (
+CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
- 
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
