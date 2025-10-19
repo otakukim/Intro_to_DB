72b3c92da1cc3ffd0c44b27e81f4110b8f313d58
otakukim <kimberlymjungwa@gmail.com>
2025-10-19 02:43:12 +0200

    Remove IF NOT EXISTS from table creation statements

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE alx_book_store;
 
 -- Creating Authors table
-CREATE TABLE IF NOT EXISTS Authors (
+CREATE TABLE  Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 -- Creating Books table
-CREATE TABLE IF NOT EXISTS Books (
+CREATE TABLE  Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS Books (
 );
 
 -- Creating Customers table
-CREATE TABLE IF NOT EXISTS Customers (
+CREATE TABLE  Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL UNIQUE,
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS Customers (
 );
 
 -- Creating Orders table
-CREATE TABLE IF NOT EXISTS Orders (
+CREATE TABLE  Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS Orders (
 );
 
 -- Creating Order_Details table
-CREATE TABLE IF NOT EXISTS Order_Details (
+CREATE TABLE  Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
